773cf7ee422eab546208616450e39df04eaf889a
8bit <yulya_kalashnikova_2002@mail.ru>
2024-06-13 22:21:19 +0700

    fourth version

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 'use strict';
 
-// switcher является ссылкой на кнопку на странике
-// querySelector использует селекторы CSS
+//Эта строка ищет элемент на веб-странице с классом "btn" и сохраняет его в переменной switcher
 const switcher = document.querySelector('.btn');
 
-// обработчик событий click
+// обработчик событий click на элементе switcher
 switcher.addEventListener('click', function() {
-    document.body.classList.toggle('light-theme');
+    /*Эти строки переключают классы 'light-theme' и 'dark-theme' для элемента <body> документа. 
+    Если у элемента уже есть класс 'light-theme', то он будет удален, а если нет, то добавлен*/
+    document.body.classList.toggle('light-theme'); // togle — удаление или изменение
     document.body.classList.toggle('dark-theme');
 
-    const className = document.body.className;
+    const className = document.body.className; // Эта строка сохраняет текущие классы элемента <body> в переменной className
     if(className == "light-theme") {
-        this.textContent = "Dark";
+        this.textContent = "Dark"; // Если тема светлая, то на кнопке будет написано Dark
     } else {
-        this.textContent = "Light";
+        this.textContent = "Light"; // Тут наоборот
     }
 
     console.log('current class name: ' + className);
